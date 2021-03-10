de0f58b7655e77ed9c772a8dd7d85ddf808bebc6
sarah-knowles <sjk339@gmail.com>
2021-03-10 21:28:04 +1300

    adds local storage

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,21 +1,44 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ setInputText, inputText, todos, setTodos, setFilter }) => {
+
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos, { text: inputText, completed: false, id: Math.random() * 1000 }
+    ]);
+    setInputText('');
+  };
+
+  const filterHandler = (e) => {
+    setFilter(e.target.value);
+  }
+
   return (
     <form>
-      <input type='text' className='todo-input' />
-      <button className='todo-button' type='submit'>
-        <i className='fas fa-plus-square'></i> 
+      <input value={inputText}
+        onChange={inputTextHandler}
+        type='text'
+        className='todo-input'
+      />
+      <button onClick={submitTodoHandler}
+        className='todo-button'
+        type='submit'>
+        <i className='fas fa-plus-square'></i>
       </button>
       <div className='select'>
-        <select name='todos' className='filter-todo'>
+        <select onChange={filterHandler} name='todos' className='filter-todo'>
           <option value='all'>all</option>
           <option value='completed'>completed</option>
           <option value='incomplete'>incomplete</option>
-        </select>  
-      </div>  
-     </form>
+        </select>
+      </div>
+    </form>
   );
-} 
+}
 
 export default Form;
